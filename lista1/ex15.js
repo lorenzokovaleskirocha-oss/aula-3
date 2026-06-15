66ca0a6e2e3dea411fb723d3f0a57552e28f4271
lorenzokovaleskirocha-oss <lorenzo.kovaleski.rocha@escola.pr.gov.br>
2026-06-15 20:51:59 +0000

    up

---
+++ lista1/ex15.js
@@ -0,0 +1,7 @@
+// 1. Cria o array com 5 produtos (objetos)
+const catalogo = [
+  { id: 1, nome: "Teclado Mecânico", preco: 150.00 },
+  { id: 2, nome: "Mouse Gamer", preco: 80.00 },
+  { id: 3, nome: "Monitor 24'", preco: 750.00 },
+  { id: 4, nome: "Suporte de Notebook", preco: 45.00 },
+  { id: 5,
