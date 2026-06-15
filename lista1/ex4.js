2d6e3a4c4b2195e1a597d76feb6554210918d80c
lorenzokovaleskirocha-oss <lorenzo.kovaleski.rocha@escola.pr.gov.br>
2026-06-15 20:41:48 +0000

    up

---
+++ lista1/ex4.js
@@ -0,0 +1,8 @@
+// Criamos uma variável que começa como uma string vazia
+let linha = "";
+
+// O laço vai rodar 7 vezes
+for (let i = 1; i <= 7; i++) {
+  linha += "#"; // Adiciona um # ao final da string atual
+  console.log(linha); // Exibe a linha atual no console
+}
