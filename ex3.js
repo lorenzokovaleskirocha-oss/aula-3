e26cea216a9bdc121fbdef3734cb7160fb830546
lorenzokovaleskirocha-oss <lorenzo.kovaleski.rocha@escola.pr.gov.br>
2026-06-15 20:33:40 +0000

    up

---
+++ ex3.js
@@ -0,0 +1,8 @@
+// 1. Declara o número a ser testado
+const numero = 7;
+
+// 2. Utiliza o operador ternário para verificar se é par ou ímpar
+const resultado = (numero % 2 === 0) ? "par" : "ímpar";
+
+// 3. Exibe o resultado usando Template String
+console.log(`O número ${numero} é ${resultado}.`);
