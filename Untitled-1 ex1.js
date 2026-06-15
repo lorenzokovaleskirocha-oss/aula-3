8cd2bdb326f00ca98f276b8828f421c7b3d96558
lorenzokovaleskirocha-oss <lorenzo.kovaleski.rocha@escola.pr.gov.br>
2026-06-15 20:28:44 +0000

    up

---
+++ Untitled-1 ex1.js
@@ -0,0 +1,8 @@
+// 1. Declara a temperatura em Celsius
+const temperaturaCelsius = 25;
+
+// 2. Converte para Fahrenheit usando a fórmula
+const temperaturaFahrenheit = temperaturaCelsius * 1.8 + 32;
+
+// 3. Exibe o resultado usando Template String
+console.log(`A temperatura de ${temperaturaCelsius}°C equivale a ${temperaturaFahrenheit}°F.`);
