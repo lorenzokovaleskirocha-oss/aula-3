86cece17dba197ec381ae4aed7364616cbed6e3f
lorenzokovaleskirocha-oss <lorenzo.kovaleski.rocha@escola.pr.gov.br>
2026-06-15 20:51:11 +0000

    up

---
+++ lista1/ex14.js
@@ -0,0 +1,6 @@
+function reverter(array) {
+  const arrayInvertido = [];
+
+  // O laço começa no último índice (array.length - 1) 
+  // e vai decrementando (i--) até chegar ao índice 0
+  for (let i = array.length -
